95817c32d9dc3aed6917de9c353f9f1bf3853502
dhan12 <emaildhan@gmail.com>
2017-08-20 11:19:50 -0400

    Make run.bash work like Makefile

--- run.bash
+++ run.bash
@@ -1,28 +1,44 @@
 #!/bin/bash
+# Wrapper script to use while developing this application.
 
+# Run given command and exit if it fails.
+# arg 1-n: The command with parameters to run.
+function run {
+    cmd="${@:1}"
+    eval $cmd
+    rc=$?
 
-function clean_and_exit {
-    deactivate
-    echo $1
+    if [[ $rc -ne 0 ]]; then
+        echo "Failed to run cmd: \"$cmd\", rc: $rc"
+        clean_and_exit $rc
+    fi
 }
 
+# Turn off virtual enviornment and exit.
+# arg 1: Is the rcode to exit with.
+function clean_and_exit {
+    deactivate
+    exit $1
+}
+
+
 if [[ "$1" = "install" ]]; then
-	virtualenv .venv
-	. .venv/bin/activate
-	pip3 install -r requirements.txt
-	pip3 install pep8 autopep8 pytest mock
-    exit 0
+	run virtualenv .venv
+	run . .venv/bin/activate
+	run pip3 install -r requirements.txt
+	run pip3 install pep8 autopep8 pytest mock
+else
+    # Set up environment
+    run . .venv/bin/activate
+    run python setup.py -q develop
+
+    # Test
+    run autopep8 --in-place -r setup.py FantasyFootball tests
+    run pep8 -r setup.py FantasyFootball tests
+    run python -m pytest tests
+
+    # Run and exit
+    run FantasyFootball $@
 fi
 
-# Set up environment
-. .venv/bin/activate
-python setup.py -q develop
-
-# Test
-autopep8 --in-place -r setup.py FantasyFootball tests
-pep8 -r setup.py FantasyFootball tests || clean_and_exit 2
-python -m pytest tests || clean_and_exit 3
-
-# Run and exit
-FantasyFootball $@
 clean_and_exit 0
